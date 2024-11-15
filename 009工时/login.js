d092e3b4366619a346cedea782b23b9c02751104
test <test@email.com>
2024-11-15 21:27:35 +0800

    记录fastapi 问题,目前伪登录接口

--- 009工时/login.js
+++ 009工时/login.js
@@ -32,14 +32,24 @@ new Vue ({
                 if(response.data.success){
                     this.message= "登录成功"
                     this.messageClass= "alert-success"
-                    window.location("./dashboard.html")
+                   
+                    window.location = "./dashboard.html";
                 }else{
                     this.message= response.data.message||'登录失败'
                     this.messageClass="alert-error"
                 }
             }catch(e){
-                            
-                this.message=e
+                   
+                // 对e 进行优化
+                if(e.response){
+
+                }else if(e.request){
+                    this.message= "网络错误"+": "+"请检查相关后台接口是否启动"
+
+                }else{
+                    this.message= "未知错误" + e.message
+                }
+              
                 this.messageClass="alert-danger"
             }
            
