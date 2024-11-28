2447f70ba19055fba673418d888d458a63c804ac
Woaini33279 <1026177564@qq.com>
2024-11-28 15:11:13 +0800

    创建初始nextjs
    将inex.js 移动到App 目录下的page.js

---
+++ 013_nextjs/App/page.js
@@ -0,0 +1,37 @@
+
+ import {useState} from 'react'  // 从react框架中导入useState
+// 复制index.html 去掉body 部分，保存js 
+     
+ 
+      function Header({ title }) {
+        return <h1>{title ? title : "Default title"}</h1>
+      }
+ 
+      export default function HomePage() {
+        const names = ["Ada Lovelace", "Grace Hopper", "Margaret Hamilton"]
+ 
+        const [likes, setLikes] = React.useState(0)
+ 
+        function handleClick() {
+          setLikes(likes + 1)
+        }
+ 
+        return (
+          <div>
+            <Header title="Develop. Preview. Ship." />
+            <ul>
+              {names.map((name) => (
+                <li key={name}>{name}</li>
+              ))}
+            </ul>
+ 
+            <button onClick={handleClick}>Like ({likes})</button>
+          </div>
+        )
+      }
+// 创建App文件夹,将其挪到App文件夹中
+
+// 将index.js 改为page.js
+
+// 将模块导出 export default function 
+
---
+++ 013_nextjs/start.sh
@@ -0,0 +1,15 @@
+# 创建空文件夹
+    mkdir 013_netxtjs
+# 配置next js npm 环境
+    npm install react@latest react-dom@latest next@latest
+## 查看package.json  安装了 next、react 和 react-dom 包，就可以开始了
+    {
+        "dependencies": {
+            "next": "^15.0.3",
+            "react": "^18.3.1",
+            "react-dom": "^18.3.1"
+        }
+    }   
+
+## 创建index.js     
+    
