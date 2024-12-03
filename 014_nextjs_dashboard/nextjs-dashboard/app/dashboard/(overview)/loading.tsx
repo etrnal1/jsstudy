11c82bdcf0f8c9b8b3f42db3e5c8cdfe75a37068
Woaini33279 <1026177564@qq.com>
2024-12-03 15:58:34 +0800

    测试
    继续测试修复

--- 014_nextjs_dashboard/nextjs-dashboard/app/dashboard/(overview)/loading.tsx
+++ 014_nextjs_dashboard/nextjs-dashboard/app/dashboard/(overview)/loading.tsx
@@ -1,9 +1,9 @@
 // 建立在Suspense之上特殊的netx.js 文件。允许回退UI
 // 导入相关组件
-import DashboardSkeleton from "../../ui/skeletons" //加载骨架，显示用户动态正在加载
+// import DashboardSkeleton from "../../ui/skeletons" //加载骨架，显示用户动态正在加载
 
 
 //loading.tsx 比其他界面高一个级别
 export default function Loading(){
-    return <DashboardSkeleton />;
+    return <p>hello world</p>;
 }
